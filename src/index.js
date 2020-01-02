31bb2be5e22be05798efa89e18c8aba56531ef51
Kelvin Liang <kelvin9877@gmail.com>
2020-01-03 00:22:49 +0800

    finish step 2 - fetch weather data and console.log

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import './style.scss';
 
-const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/'
 
 const getWeather = async (city) => {
+  const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/'
   const url = WEATHER_BASE_URL + `weather?q=${city}` + '&appid=' + `${process.env.OPEN_WEATHER_API_KEY}`;
 
   const response = await fetch(url, { mode: 'cors' });
-  console.log(response);
   const data = await response.json();
+  return data;
+};
 
-  return data.data;
-}
+getWeather('London')
+  .then(x => console.log(x));
 
-
-const weather = getWeather('London');
-
-console.log(weather);

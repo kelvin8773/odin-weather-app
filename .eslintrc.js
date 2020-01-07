7a1607f4e6482ec86bf2fc60c9c340a16a32c3f4
Kelvin Liang <kelvin9877@gmail.com>
2020-01-07 19:38:09 +0800

    add local eslinter config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "parserOptions": { "ecmaVersion": 2018 },
+  "extends": "airbnb-base",
+  rules: {
+    'no-shadow': 0,
+    'no-param-reassign': 0,
+    'eol-last': 0,
+  }
+}
